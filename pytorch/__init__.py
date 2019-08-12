d20dc57afeb07e8867c3da8f44ec5e37e84ce3d4
kzxuan <wuxixkz@sina.com>
2019-08-12 21:38:42 +0800

    rebuild

--- pytorch/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Deep neural model written by PyTorch
-Ubuntu 16.04 & PyTorch 1.0
-Last update: KzXuan, 2019.04.09
-Version 0.12.3
-"""
--- utils/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Utilities
-Last update: KzXuan, 2019.04.10
-"""
